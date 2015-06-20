211a20f619d282b5690fabac2921f7741277bebe
Kaue Silveira <silveira.kaue@gmail.com>
2015-06-20 15:59:27 +0200

    2 missing

--- 2015/m/m.py
+++ 2015/m/m.py
@@ -57,10 +57,27 @@ for n in range(10, 1001):
         # print "found", i, test
         res[n] = test
 
+for n in range(10, 1001):
+  for i in range(2, n/2 + 1):
+    if n % i == 0:
+      test = "[%s]*[%s]" % (smaller(res[i]), smaller(res[n / i]))
+      if len(test) < len(res[n]):
+        # print "found", i, test
+        res[n] = test
+  for i in range(1, n - 0):
+    if not res[n - i].endswith('-[]'):
+      right = res[n - i]
+      if right.startswith('+'):
+        right = right[1:]
+      test = "%s+%s" % (((res[i])), (right))
+      if len(test) < len(res[n]):
+        # print "found", i, test
+        res[n] = test
+
 for i, s in res.iteritems():
   if len(s) > 75:
     sys.stderr.write("%s, %s\n" % (i, s))
-    # print i, s  # 264, 235, 45, 44, 8
+    # print i, s  # 264, 235, 45, 44, 8, 2
   # assert len(s) <= 75
   # print s
   print "console.log(%s);" % s
