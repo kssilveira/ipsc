49f9fecd86576e4f8ebf16dc4093961b79ffb88d
Kaue Silveira <silveira.kaue@gmail.com>
2015-06-20 17:03:53 +0200

    d solved easy

--- 2015/d/sol_d.py
+++ 2015/d/sol_d.py
@@ -6,7 +6,8 @@ random.seed(seed)
 n = 60
 m = 0
 
-edge_prob = random.randint(1, 1000000)
+edge_prob = 10
+negative_prob = 10
 
 edges = {}
 
@@ -14,7 +15,9 @@ for i in range(n):
   edges[i] = {}
   for j in range(i + 1, n):
     if random.randint(0, edge_prob):
-      edges[i][j] = random.randint(-(2**31), (2**31) - 1)
+      edges[i][j] = random.randint(1, 2**30)
+      if random.randint(0, negative_prob):
+        edges[i][j] = -edges[i][j]
       m += 1
 
 print n
